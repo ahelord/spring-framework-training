dff7598dab9adb63c388b2f4b2caa540bb98be0f
Leonardo Rodriguez <ahelord@gmail.com>
2023-10-01 17:44:19 -0500

    feat: properties

--- spring-framework-java8-maven/src/main/java/com/ahelord/springtraining/helloworld/HelloController.java
+++ spring-framework-java8-maven/src/main/java/com/ahelord/springtraining/helloworld/HelloController.java
@@ -1,4 +1,4 @@
-package com.ahelord.springtraining;
+package com.ahelord.springtraining.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
